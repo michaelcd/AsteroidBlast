cfc512b9be36d7e3df7d9cb51055bc4caedda66a
App Academy Student <appacademy@Apps-Mac-mini-5.local>
2016-01-11 17:41:17 -0500

    Asteroid movement and wrapping implemented.

---
+++ asteroids/lib/asteroid.js
@@ -0,0 +1,20 @@
+(function () {
+
+  if (typeof window.Asteroids === "undefined") {
+    window.Asteroids = {};
+  }
+
+  var Asteroids = window.Asteroids;
+
+  var Asteroid = Asteroids.Asteroid = function (properties) {
+    Asteroids.MovingObject.call(this,
+      { game: properties.game, pos: properties.pos, color: "#00FF00", radius: 4,
+      vel: Asteroids.Util.randomVec()}
+    ); // need velocity
+  };
+
+  Asteroids.Util.inherits(Asteroid, Asteroids.MovingObject);
+
+
+
+})();
---
+++ asteroids/lib/gameView.js
@@ -0,0 +1,23 @@
+(function () {
+
+  if (typeof window.Asteroids === "undefined") {
+    window.Asteroids = {};
+  }
+
+  var GameView = Asteroids.GameView = function (ctx) {
+    this.game = new Asteroids.Game();
+    this.ctx = ctx;
+  };
+
+  GameView.prototype.start = function () {
+    var gameview = this;
+    window.setInterval(function () {
+      gameview.game.moveObjects();
+      gameview.game.draw(this.ctx);
+    }, 20);
+  };
+
+
+
+
+})();
